a5ed4b7d64f14a62432788f999e9aac94f281f21
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-12-15 13:37:34 -0600

    Refactor rectangle class and method naming
    Refactor rectangle class to use PascalCase and update method signature.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-
 let length = 5;
-let height = 10;
-class rectangle {
+let height = 10; 
+
+class Rectangle {
+
     constructor(length,height) {
         this.length = length;
         this.height = height;
     };
-    calculateArea() {
+    calculateArea(length, height) {
         return this.length * this.height;
     };
 };
 document.getElementById('result').innerHTML = rectangle;
+
 
